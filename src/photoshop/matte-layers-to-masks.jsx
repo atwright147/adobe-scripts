4a7886292e8eb29beb11691c0e0e2abf023d53c5
Andy Wright <atwright147@gmail.com>
2022-07-13 23:28:19 +0100

    initial commit

---
+++ src/photoshop/matte-layers-to-masks.jsx
@@ -0,0 +1,10 @@
+/// <reference types="types-for-adobe/Photoshop/2015.5"/>
+
+alert(String(app));
+
+var doc = app.activeDocument;
+doc.layers;
+doc.layers[0];
+
+
+// doc.layers.getByName("Background");
