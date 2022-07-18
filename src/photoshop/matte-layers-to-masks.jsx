144a730424a4f159c264f8210354cdd4607f6324
Andy Wright <atwright147@gmail.com>
2022-07-18 19:48:38 +0100

    create initial version of matte layers to masks script

--- src/photoshop/matte-layers-to-masks.jsx
+++ src/photoshop/matte-layers-to-masks.jsx
@@ -1,10 +1,43 @@
 /// <reference types="types-for-adobe/Photoshop/2015.5"/>
 
-alert(String(app));
+// #target 'photoshop-160.064'
+// #targetengine 'transient'
 
-var doc = app.activeDocument;
-doc.layers;
-doc.layers[0];
+app.displayDialogs = DialogModes.NO;
 
+const allLayers = app.activeDocument.layers;
 
-// doc.layers.getByName("Background");
+// hide all layers
+for (var i = 0; i < allLayers.length; i++) {
+    app.activeDocument.layers[i].visible = false;
+}
+
+// create masks
+for (var i = 0; i < allLayers.length; i++) {
+    var layerName = app.activeDocument.layers[i].name;
+
+    if (/Matte/.test(layerName)) {
+        try {
+            app.activeDocument.layers[i].visible = true;
+
+            var doc = app.activeDocument;
+            var channelRef = doc.channels.getByName("Red");
+            doc.selection.load(channelRef, SelectionType.REPLACE);
+            $.writeln(doc.selection.bounds);
+
+            var selAlpha = app.activeDocument.channels.add();
+            app.activeDocument.selection.store(selAlpha);
+            app.activeDocument.selection.deselect();
+            selAlpha.name = layerName;
+
+            app.activeDocument.layers[i].visible = false;
+        } catch (err) {
+            // do nothing
+        }
+    }
+}
+
+// show all layers
+for (var i = 0; i < allLayers.length; i++) {
+    app.activeDocument.layers[i].visible = true;
+}
